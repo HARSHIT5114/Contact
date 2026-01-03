691fd9460dd20b914841f1015bda20a666af0db1
HARSHIT5114 <rajputharshit5114@gmail.com>
2026-01-03 12:49:09 +0530

    Use environment variable for frontend URL

--- contact-app/backend/app.js
+++ contact-app/backend/app.js
@@ -1,12 +1,14 @@
 import express from "express";
 import cors from "cors";
 import contactRoutes from "./routes/contactRoutes.js";
+import dotenv from "dotenv"; 
 
 const app = express();
+dotenv.config();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST", "DELETE"],
     allowedHeaders: ["Content-Type"],
   })
